8b2c87eb985db355897c65731abb91943fcddd59
吴小白 <296015668@qq.com>
2026-04-25 17:15:34 +0800

    Enable loongarch64-linux build
    Signed-off-by: 吴小白 <296015668@qq.com>

--- src/release.rs
+++ src/release.rs
@@ -187,6 +187,20 @@ pub static RELEASE_TRIPLES: Lazy<BTreeMap<&'static str, TripleRelease>> = Lazy::
     );
 
     h.insert(
+        "loongarch64-unknown-linux-gnu",
+        TripleRelease {
+            suffixes: linux_suffixes_nopgo.clone(),
+            install_only_suffix: "lto",
+            freethreaded_install_only_suffix: "freethreaded+lto",
+            python_version_requirement: Some(VersionSpecifier::from_str(">=3.10").unwrap()),
+            conditional_suffixes: vec![ConditionalSuffixes {
+                python_version_requirement: VersionSpecifier::from_str(">=3.13").unwrap(),
+                suffixes: linux_suffixes_nopgo_freethreaded.clone(),
+            }],
+        },
+    );
+
+    h.insert(
         "ppc64le-unknown-linux-gnu",
         TripleRelease {
             suffixes: linux_suffixes_nopgo.clone(),
