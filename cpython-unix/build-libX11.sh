e8d8c8b0b405403a0f879b89f434e0bffe4ee110
Jonathan J. Helmus <jjhelmus@gmail.com>
2026-06-16 11:19:06 -0600

    update X.org dependencies (#1157)
    * libX11 1.6.12 -> 1.8.13
    * libXau 1.0.11 -> 1.0.12
    * xorgproto 2024.1 -> 2025.1

--- cpython-unix/build-libX11.sh
+++ cpython-unix/build-libX11.sh
@@ -13,30 +13,6 @@ export PKG_CONFIG_PATH=/tools/deps/share/pkgconfig:/tools/deps/lib/pkgconfig
 tar -xf "libX11-${LIBX11_VERSION}.tar.gz"
 pushd "libX11-${LIBX11_VERSION}"
 
-patch -p1 << 'EOF'
-diff --git a/configure b/configure
---- a/configure
-+++ b/configure
-@@ -19557,8 +19557,6 @@ else
- 		RAWCPPFLAGS="-undef -ansi"
- 		{ $as_echo "$as_me:${as_lineno-$LINENO}: result: yes, with -ansi" >&5
- $as_echo "yes, with -ansi" >&6; }
--	else
--		as_fn_error $? "${RAWCPP} defines unix with or without -undef.  I don't know what to do." "$LINENO" 5
- 	fi
- fi
- rm -f conftest.$ac_ext
-@@ -19578,8 +19576,6 @@ else
- 		RAWCPPFLAGS="${RAWCPPFLAGS} -traditional"
- 		{ $as_echo "$as_me:${as_lineno-$LINENO}: result: yes" >&5
- $as_echo "yes" >&6; }
--	else
--		as_fn_error $? "${RAWCPP} does not preserve whitespace with or without -traditional.  I don't know what to do." "$LINENO" 5
- 	fi
- fi
- rm -f conftest.$ac_ext
-EOF
-
 if [ "${CC}" = "musl-clang" ]; then
     EXTRA_FLAGS="--disable-shared"
 fi
@@ -112,12 +88,15 @@ EXTRA_FLAGS="${EXTRA_FLAGS} --disable-loadable-xcursor"
 
 # CC_FOR_BUILD is here because configure doesn't look for `clang` when
 # cross-compiling. So we force it.
+# RAWCPP is X.Org's stdin-fed preprocessor command. Use the full path to
+# the target compiler driver with "-" to read from stdin.
 CFLAGS="${EXTRA_TARGET_CFLAGS} -fPIC -I/tools/deps/include" \
   CPPFLAGS="${EXTRA_TARGET_CFLAGS} -fPIC -I/tools/deps/include" \
   LDFLAGS="${EXTRA_TARGET_LDFLAGS}" \
   CC_FOR_BUILD="${HOST_CC}" \
   CFLAGS_FOR_BUILD="-I/tools/deps/include" \
   CPPFLAGS_FOR_BUILD="-I/tools/deps/include" \
+  RAWCPP="$(command -v "${CC}") -E -"\
   ./configure \
     --build="${BUILD_TRIPLE}" \
     --host="${TARGET_TRIPLE}" \
